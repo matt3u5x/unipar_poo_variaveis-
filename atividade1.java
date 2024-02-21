da13a329b999c6446a2fb631cd68d57c8111732b
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-02-20 22:17:27 -0300

    Update atividade1.java

--- atividade1.java
+++ atividade1.java
@@ -1,16 +1,17 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+            String nomeAluno;
+            int nota1;
+            int nota2;
+            double notasMedia;
+            String cidadeAL;
+            String ra;
+            float idade;
+            float peso;
+            boolean matriculado;
+            boolean presente;
+
     }
 }
 
-String NomeAluno;
-int nota1;
-int nota2;
-double notasMedia;
-String cidadeAL;
-String RA;
-float idade;
-float peso;
-boolean matriculado;
-boolean presente;
+
