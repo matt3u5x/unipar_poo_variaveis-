d21e62fe3ea9c1ea273d9aad4e4e79c4f7ea4fa0
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-02-20 21:54:52 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+a. Nomeproduto
+b. Precoproduto
+c. Marcadoproduto
+d. Quantidadeproduto
+e. Descricaoproduto
+f. Nomeproduto
+g. Numeroproduto
+h. Precodoproduto
+i. Estoquedisponivel
+j. Totaldeprodutos 
