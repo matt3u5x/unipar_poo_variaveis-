16e6441c41b1d83a99e2a0abfe5e8854c8fcfd91
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-02-20 22:17:56 -0300

    Update atividade2.java

--- atividade2.java
+++ atividade2.java
@@ -1,15 +1,15 @@
 class HelloWorld {
     public static void main(String[] args) {
-    a. Nomeproduto
-    b. Precoproduto
-    c. Marcadoproduto
-    d. Quantidadeproduto
-    e. Descricaoproduto
-    f. Nomeproduto
-    g. Numeroproduto
-    h. Precodoproduto
-    i. Estoquedisponivel
-    j. Totaldeprodutos 
+    a. nomeproduto
+    b. precoproduto
+    c. marcadoproduto
+    d. quantidadeproduto
+    e. descricaoproduto
+    f. nomeproduto
+    g. numeroproduto
+    h. precodoproduto
+    i. estoquedisponivel
+    j. totaldeprodutos 
     }
 }
 
