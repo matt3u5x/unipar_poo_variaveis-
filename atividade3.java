734725a3f55a17e0a40394db45c81ae90a368df6
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-02-20 22:02:19 -0300

    Create atividade3.java

---
+++ atividade3.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+String Nomedoaluno;
+int Idadealuno;
+float Nota1
+float Nota2
+double Medfinal
