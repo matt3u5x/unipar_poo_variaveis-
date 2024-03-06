a44182fbb37013ac86db4d20c8bf88a43d3605db
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-03-05 21:16:26 -0300

    Create atividade6.java

---
+++ atividade6.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String numeroProduto = "A21";
+        double precoProduto = 12;
+        
+        String descricaoProduto;
+        
+        descricaoProduto = "Fone JBL";
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+        
+        System.out.println("Dados do Produto");
+        System.out.println("Numero:" + numeroProduto);
+        System.out.println("preco: $" + precoProduto);
+        System.out.println("Descao: " + descricaoProduto);
