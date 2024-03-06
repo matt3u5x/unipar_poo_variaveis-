cf34ebcacba362840383a6ba3ad8272bac10bf10
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-03-05 21:14:50 -0300

    Create atividade5.java

---
+++ atividade5.java
@@ -0,0 +1,39 @@
+class HelloWorld {
+    public static void main(String[] args) {
+ 
+    int estoqueSmarthphone = 12;
+    double precoSmartphone = 1700.00;
+    String descricaoSmartphone = "Tecnologico";
+    String dadosSmartphone = "Kit Iphone";
+    
+    int numeroLaptop = 13;
+    double precoLaptop = 2300.00;
+    String descricaoLaptop = "Inovador";
+    String dadosLaptop = "Laptop Positivo";
+    
+    int numeroFone = 15;
+    double precoFone = 500.00;
+    String descricaoFone = "Preto";
+    String dadosFone = "Fone de ouvido Samsung";
+    
+    System.out.println("SmarthPhone:");
+    System.out.println("Preco: " + precoSmartphone);
+    System.out.println("Descricao: " + descricaoSmartphone);
+    System.out.println("Dados: " + dadosSmartphone);
+    System.out.println();
+    
+    System.out.println("Laptop:");
+    System.out.println("Preco:" + precoLaptop);
+    System.out.println("Descricao: " + descricaoLaptop);
+    System.out.println("Dados: " + dadosLaptop);
+    System.out.println();
+    
+    System.out.println("Fone de Ouvido:");
+    System.out.println("Preco: " + precoFone);
+    System.out.println("Descricao: " + descricaoFone);
+    System.out.println("Dados: " + dadosFone);
+    System.out.println();
+    
+     
+    }
+}
