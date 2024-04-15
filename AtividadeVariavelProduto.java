b0c7312882db2232506b622802d2dc56d967a57f
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-04-15 12:06:17 -0300

    Create AtividadeVariavelProduto.java

---
+++ AtividadeVariavelProduto.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nomeProduto
+        int precoProduto
+        String marcadoProduto
+        int quantidadeProduto
+        String _descricaoProduto
+        String nomeProduto
+        Int _numeroProduto
+        float precodoProduto
+        int estoqueDisponivel
+        int totaldeProdutos
+
+    }
+}
