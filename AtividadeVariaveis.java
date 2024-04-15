db8149b10c67810f520d2de9e9ce693f8bc78ceb
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-04-15 12:09:03 -0300

    Create AtividadeVariaveis.java

---
+++ AtividadeVariaveis.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nomeAluno;
+        String cidadeAluno;
+        int rgAluno;
+        int cpfAluno;
+        int raAluno;
+        int cepCidade;
+        float pesoAluno;
+        float tamanhoAluno;
+        boolean matriculado;
+        boolean presente;
+     
+    }
+}
