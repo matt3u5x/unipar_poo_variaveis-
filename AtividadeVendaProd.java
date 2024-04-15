f36a993524cab38c0be197fb04be4982b476f7c2
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-04-15 12:05:08 -0300

    Create AtividadeVendaProd.java

---
+++ AtividadeVendaProd.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int numeroProduto = 123 ; 
+        double precoProduto = 25.00; 
+        String descricaoProduto = "Elegante";
+        String detalhesProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+        System.out.println("Detalhes: " + detalhesProduto);
+    }
+}
