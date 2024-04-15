efaa0572cffd4fa27fd93cdadf205d25186b9e66
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-04-15 12:05:42 -0300

    Create AtividadeVendaProduto2.java

---
+++ AtividadeVendaProduto2.java
@@ -0,0 +1,15 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+     
+        String nomeProduto = "Fone de Ouvido";
+        double precoProduto = 29.99;
+        int quantidadeProduto = 10;
+        boolean marcaProduto = true;
+
+        System.out.println("Dados do Produto");
+        System.out.println("Nome: " + nomeProduto);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preco: $ " + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
