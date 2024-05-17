11380e929e6e5b73a539b5600692c8cd5515eeca
matt3u5x <160682471+matt3u5x@users.noreply.github.com>
2024-05-17 10:17:36 -0300

    Create Atividade3Heranca.java

---
+++ Atividade3Heranca.java
@@ -0,0 +1,72 @@
+package br.unipar;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.facaSom();
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.facaSom();
+
+        Calculadora calculadora = new Calculadora();
+        int sum1 = calculadora.add(1,2);
+        System.out.println(sum1);
+        int sum2 = calculadora.add(1,2,3);
+        System.out.println(sum2);
+        double sum3 = calculadora.add(1.5,2.5);
+        System.out.println(sum3);
+    }
+}
+
+/*Explicação: Override é usada para indicar que o método facaSom() na classe Gato está sobrescrevendo o método facaSom() da classe Animal, que é a classe pai de Gato.
+    A classe pai, temos o método facoSom(), que chama "O animal faz o som" assim que chamado. Na classe Gato temos outro método facoSom() que representa o "Meow" assim que chamado.
+    O Override antes do método facaSom() na classe Gato indica que este método está substituindo o método facaSom() da classe Animal.
+    Isso significa que, quando você tem um objeto da classe Gato e chama o método facaSom(), o método facaSom() da classe Gato será executado, não o da classe Animal.
+    No método main(), um objeto chamado animal é criado a partir da classe Animal e um objeto chamado gato é criado a partir da classe Gato. Quando o método facaSom() é chamado no objeto animal, a saída é “O animal faz o som”.
+    Isso ocorre porque esse é o comportamento definido para o método facaSom() na classe Animal. No entanto, quando o método facaSom() é chamado no objeto gato, a saída é “Meow”.
+    Isso ocorre porque a classe Gato, que é uma subclasse da classe Animal, sobrescreveu o método facaSom() para exibir “Meow” em vez de “O animal faz o som”.*/
+    
+------------
+
+package br.unipar;
+
+public class Animal {
+    public void facaSom(){
+        System.out.println("Som");
+    }
+}
+
+------------
+------------
+
+  package br.unipar;
+
+public class Cachorro extends Animal {
+    @Override
+    public void facaSom(){
+        System.out.println("Au-ar-au");
+    }
+
+}
+
+------------
+
+package br.unipar;
+
+public class Calculadora {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
+
+/*Explicação: A sobrecarga é aplicada na classe Calculadora.
+    O método add(int a, int b) recebe dois inteiros e retorna a soma deles.
+    O método add(int a, int b, int c) também se chama add, mas recebe três inteiros e retorna a soma deles.
+    O método add(double a, double b) é mais um método add, mas desta vez recebe dois números de ponto flutuante (double) e retorna a soma deles.
+    Cada um desses métodos add realiza a mesma operação básica (adição), mas eles são aplicados a diferentes números de argumentos ou tipos de argumentos.*/
